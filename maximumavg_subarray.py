5d0a104d4b72fb3ea233524c76bf6ff687e5615c
Shrushti Jadhav <shrushtipj2610@gmail.com>
2026-05-29 23:12:23 +0530

    Added maximum avgerage subarray  solution

---
+++ maximumavg_subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+        for i in range(k, len(nums)):
+            window_sum += nums[i] - nums[i - k]  # add new, remove old
+            max_sum = max(max_sum, window_sum)
+        return max_sum / k 
