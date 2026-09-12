1a6353b85959251325b42424d13060093008a62a
Shrushti Jadhav <shrushtipj2610@gmail.com>
2026-09-12 21:20:47 +0530

    longest common prefix Array

---
+++ longestcommonprefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       string ans="";
+       for(int j = 0; j < strs[0].size(); j++){
+        for(int i=0;i<strs.size();i++){
+                    if(j >= strs[i].size() || strs[i][j] != strs[0][j]) {
+            return ans;
+        }
+    }
+
+    ans += strs[0][j];
+    }
+
+return ans;
+    }
+};
